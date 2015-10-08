23645500e439dc55220116051f51514e1f46007a
Nick Semenkovich <semenko@alum.mit.edu>
2015-10-08 13:34:37 -0500

    Working legacy code.

--- legacy-blockingWebRequest/background.js
+++ legacy-blockingWebRequest/background.js
@@ -1,5 +1,11 @@
 /*
- Force PDFs to render inline in Chrome.
+ Force PDFs to render inline in Chrome (for the Stable channel).
+
+ Note: This is a slower version than the Declarative Web Request extension at:
+ https://chrome.google.com/webstore/detail/render-pdfs-inline/mpmmilbhemhehclnkpkfepmaikiolaab
+
+ (Unfortunately, the DWR API isn't available on the Stable channel.)
+
 
  WARNING: This may have significant security implications. If you don't understand why, do NOT install this extension.
 
@@ -9,16 +15,18 @@
 
 chrome.webRequest.onHeadersReceived.addListener(
     function(details) {
-        console.log(details);
         var responseHeaders = details.responseHeaders;
-        console.log(responseHeaders);
         responseHeaders.forEach(function(header){
             switch(header.name.toLowerCase()) {
                 case 'content-disposition':
-                    if (header.name.toLowerCase() == 'content-disposition') {
-                        header.value = header.value.replace('attachment;', 'inline;');
-                        console.log('Injecting inline header for:', details)
-                    }
+                    header.value = header.value.replace('attachment', 'inline');
+                    console.log('Injecting Content-Disposition inline header for:', details);
+                    break;
+                case 'content-type':
+                    // This might break stuff, but if you name your file PDF, we're gonna force it to be a PDF.
+                    // "Whoops"
+                    header.value = "application/pdf";
+                    console.log('Injecting Content-Type PDF header for:', details);
                     break;
             }
         });
@@ -26,68 +34,16 @@ chrome.webRequest.onHeadersReceived.addListener(
     },
     // Rather than intercept and parse all headers for content-type (slow, but catches ~everything)
     // we prefer to catch only PDF named files, as a faster best-effort for inline.
-    {urls: ["*://*/*.pdf",
-	    "*://*/*.Pdf",
-	    "*://*/*.pDf",
-	    "*://*/*.pdF",
-	    "*://*/*.PDf",
-	    "*://*/*.PdF",
-	    "*://*/*.pDF",
-	    "*://*/*.PDF"]},
+    {
+        urls: ["*://*/*.pdf",
+            "*://*/*.Pdf",
+            "*://*/*.pDf",
+            "*://*/*.pdF",
+            "*://*/*.PDf",
+            "*://*/*.PdF",
+            "*://*/*.pDF",
+            "*://*/*.PDF"],
+        types: ["main_frame", "sub_frame"]
+    },
     ["blocking", "responseHeaders"]
 );
-
-/*
-var rule = {
-    conditions: [
-        new chrome.declarativeWebRequest.RequestMatcher({
-            resourceType: ['main_frame'], // Only rewrite if the PDF is the whole window, not some iframe, etc.
-            contentType: ['application/pdf'],
-            responseHeaders: [{nameEquals: 'Content-Disposition', valueContains: 'attachment'}],
-            stages: ["onHeadersReceived"]
-        }),
-        new chrome.declarativeWebRequest.RequestMatcher({
-            resourceType: ['main_frame'], // Only rewrite if the PDF is the whole window, not some iframe, etc.
-            contentType: ['application/octet-stream'],
-            responseHeaders: [
-                {nameEquals: 'Content-Disposition', valueContains: '.pdf'}
-            ],
-            stages: ["onHeadersReceived"]
-        })
-    ],
-    actions: [
-        new chrome.declarativeWebRequest.RemoveResponseHeader({name: 'Content-Disposition'}),
-        new chrome.declarativeWebRequest.AddResponseHeader({name: 'Content-Disposition', value: 'inline'}),
-        new chrome.declarativeWebRequest.RemoveResponseHeader({name: 'Content-Type'}),
-        new chrome.declarativeWebRequest.AddResponseHeader({name: 'Content-Type', value: 'application/pdf'}),
-        // Uncomment for debug logging. See bottom of this file for the listener.
-        // new chrome.declarativeWebRequest.SendMessageToExtension({message: 'Rewriting PDF header.'})
-    ]};
-
-
-function addRule() {
-    chrome.declarativeWebRequest.onRequest.addRules([rule]);
-}
-
-// This runs on first install & after version updates, etc.
-function setup() {
-    // Remove all rules and add ours back in.
-    chrome.declarativeWebRequest.onRequest.removeRules(null,
-        function() {
-            if (chrome.runtime.lastError) {
-                console.error('Error clearing rules: ' + chrome.runtime.lastError);
-            } else {
-                addRule();
-            }
-        });
-}
-
-// Set some onInstalled listeners to fire, since we're not a persistent background page.
-chrome.runtime.onInstalled.addListener(setup);
-/*
-
-
- */
-// Enable this line (and the rule to SendMessage) if you're debugging.
-// chrome.declarativeWebRequest.onMessage.addListener(function callback(details) { console.log(details.message + ' ' + details.stage + ' ' + details.requestId); });
-
